6e2bd20de684e82d02ad252bdbe3a6e4c7141c58
Stefan Vermaas <stefanvermaas1991@gmail.com>
2019-07-08 13:08:57 +0200

    Adds the ESLint config and the ignore file

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
